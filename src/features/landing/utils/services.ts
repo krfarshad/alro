0d3a7ca29ca614bc7fa6e91615742d7d6d628f81
Farshad <krfarshad@gmail.com>
2023-12-24 14:22:39 +0330

    chore: add static service items

---
+++ src/features/landing/utils/services.ts
@@ -0,0 +1,32 @@
+import { Service } from "../types/type";
+
+export const ourServices: Service[] = [
+  {
+    id: 101,
+    title: "View Online",
+    text: "Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    slug: "project-1",
+    icon: "",
+  },
+  {
+    id: 101,
+    title: "Work Environment Design",
+    text: "Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    slug: "project-2",
+    icon: "",
+  },
+  {
+    id: 101,
+    title: "Exterior Design",
+    text: "Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    slug: "project-3",
+    icon: "",
+  },
+  {
+    id: 101,
+    title: "Interior Design",
+    text: "Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    slug: "project-4",
+    icon: "",
+  },
+];
