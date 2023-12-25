f9481900b017c78ef6a9e570886911201fa59205
Farshad <krfarshad@gmail.com>
2023-12-25 15:28:38 +0330

    style: edit menu item style

--- src/components/Layout/Header/Navigation/index.tsx
+++ src/components/Layout/Header/Navigation/index.tsx
@@ -2,14 +2,14 @@ import { AppRoutes } from "@/router/AppRoutes";
 import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
-  const routes = AppRoutes;
   return (
     <ul>
-      {routes.map((route) => (
-        <li className="inline-block" key={route.to}>
+      {AppRoutes.map((route) => (
+        <li className="inline-block relative" key={route.to}>
           <NavLink
             to={route.to}
-            className={`py-1 px-3 text-sm hover:text-orange-400 transition-all hover:transition-all`}
+            className={`py-1 px-5 text-white text-sm transition-all hover:transition-all after:content-[''] after:w-1 after:h-1 after:rounded-full 
+             after:opacity-0 hover:after:opacity-100  after:bg-primary  after:absolute after:-bottom-2 after:left-[50%] after:translate-x-[-50%] `}
           >
             {route.title}
           </NavLink>
