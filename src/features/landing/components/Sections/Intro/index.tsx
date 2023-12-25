995749dee61ffaa3a58c94b8b0f1cc48a59791b3
Farshad <krfarshad@gmail.com>
2023-12-25 12:24:42 +0330

    style: edit style intro section

--- src/features/landing/components/Sections/Intro/index.tsx
+++ src/features/landing/components/Sections/Intro/index.tsx
@@ -2,18 +2,20 @@ import { Title, Text, Row, Container } from "@/components";
 import introImage from "../../../assets/hero.jpg";
 const Intro = () => {
   return (
-    <Container className="py-20">
+    <Container className="py-28">
       <Row>
         <div className="w-full md:w-1/2 px-2">
-          <Title tag="h2">Welcome We Are Alro</Title>
+          <Title tag="h2" className="mb-4">
+            Welcome We Are Alro
+          </Title>
           <Text>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
           </Text>
-          <Row>
-            <ul>
+          <div className="my-4">
+            <ul className="px-4 text-secondary text-sm grid grid-cols-2 gap-4 list-disc leading-10">
               <li>Payment in installments</li>
               <li>Payment in installments</li>
               <li>Payment in installments</li>
@@ -21,10 +23,12 @@ const Intro = () => {
               <li>Payment in installments</li>
               <li>Payment in installments</li>
             </ul>
-          </Row>
+          </div>
           <Text>
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat
+            nisi ut aliquip ex ea commodo consequat . Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat
           </Text>
         </div>
         <div className="w-full md:w-1/2 px-2">
