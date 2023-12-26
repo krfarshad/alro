2b66366b1d3ad7b03c845202c8ba9fafa09b0822
Farshad <krfarshad@gmail.com>
2023-12-26 17:27:38 +0330

    style: improve hero style

--- src/features/landing/components/Sections/Hero/index.tsx
+++ src/features/landing/components/Sections/Hero/index.tsx
@@ -6,12 +6,12 @@ import logoMain from "@/assets/images/main-logo.png";
 const Hero = () => {
   return (
     <div
-      className="relative h-[630px] bg-slate-500 -mt-20 bg-no-repeat bg-cover bg-center"
+      className="relative h-[680px] bg-slate-500 -mt-20 bg-no-repeat bg-cover bg-center"
       style={{ backgroundImage: `url(${heroBg})` }}
     >
-      <Container className="flex justify-start items-center pt-20">
+      <Container className="flex justify-start items-center h-full ">
         <div className="overlay w-full lg:w-1/2">
-          <div className="logo">
+          <div className="logo mb-8">
             <img src={logoMain} alt="Alro" />
           </div>
           <div className="desciption">
@@ -21,10 +21,16 @@ const Hero = () => {
             </Title>
           </div>
           <div className="more text-right mt-8">
-            <Button type="primary" className="!text-sm !text-white">
+            <Button
+              type="primary"
+              className="!text-sm !text-white group hover:!text-primary"
+            >
               <Link className="flex justify-center" to="/about/">
                 Read more
-                <ArrowRight size="20" className="text-white ml-1" />
+                <ArrowRight
+                  size="20"
+                  className="text-white ml-1 group-hover:!text-primary transition-all hover:transition-all"
+                />
               </Link>
             </Button>
           </div>
