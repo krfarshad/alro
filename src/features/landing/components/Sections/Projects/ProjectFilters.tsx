91ba94c482121d114389278f0b8ff38d16cf28a5
Farshad <krfarshad@gmail.com>
2023-12-26 16:42:17 +0330

    refactor: add primary project filter item

--- src/features/landing/components/Sections/Projects/ProjectFilters.tsx
+++ src/features/landing/components/Sections/Projects/ProjectFilters.tsx
@@ -1,13 +1,31 @@
+import { Text } from "@/components";
+import { ArrowRight2 } from "iconsax-react";
+
 const ProjectFilters = () => {
   return (
-    <>
-      {/* size */}
-      {/* bedrooms */}
-      {/* theme */}
+    <div className="p-2">
+      <div className="flex items-center py-2 border-b border-slate-200">
+        <ArrowRight2 size="18" className="text-secondary" />
+        <Text>Size of the house</Text>
+        <div className="filter-box"></div>
+      </div>
+      <div className="flex items-center py-2 border-b border-slate-200">
+        <ArrowRight2 size="18" className="text-secondary" />
+        <Text>Number of beds</Text>
+        <div className="filter-box"></div>
+      </div>
 
-      {/* style */}
-      {/* style */}
-    </>
+      <div className="flex items-center py-2 border-b border-slate-200">
+        <ArrowRight2 size="18" className="text-secondary" />
+        <Text>Choose theme</Text>
+        <div className="filter-box"></div>
+      </div>
+      <div className="flex items-center py-2 border-b border-slate-200">
+        <ArrowRight2 size="18" className="text-secondary" />
+        <Text>Color Scheme</Text>
+        <div className="filter-box"></div>
+      </div>
+    </div>
   );
 };
 
--- src/features/projects/types/index.ts
+++ src/features/projects/types/index.ts
@@ -1,7 +1,14 @@
-export interface ProjectItem {
+export interface projectEnum {
+  size: number;
+  beds: number;
+  theme: "Modern" | "Traditional" | "classical";
+  colorScheme: "Neutral" | "Accent";
+}
+export interface ProjectItem extends projectEnum {
   id: number;
   title: string;
   image: string;
   subTitle: string;
+  description: string;
   slug: string;
 }
