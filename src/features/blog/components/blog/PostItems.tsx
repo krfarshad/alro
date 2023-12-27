5e3435152a6e3b20232ef51b26090b9aecd11374
Farshad <krfarshad@gmail.com>
2023-12-27 15:51:53 +0330

    chore: add key for mapping

--- src/features/blog/components/blog/PostItems.tsx
+++ src/features/blog/components/blog/PostItems.tsx
@@ -9,7 +9,9 @@ const PostItems = () => {
     throw new Error("Fetch Error Data");
   }
 
-  return <Row>{data && data.map((post) => <Card post={post} />)}</Row>;
+  return (
+    <Row>{data && data.map((post) => <Card key={post.id} post={post} />)}</Row>
+  );
 };
 
 export default PostItems;
