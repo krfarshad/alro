c1f59199839db05dce9c7d3f3a076bf67e7257ff
krfarshad <krfarshad@gmail.com>
2023-12-29 16:40:15 +0330

    refactor: pass direct as a internal component

--- src/components/Fields/RangeInput/index.tsx
+++ src/components/Fields/RangeInput/index.tsx
@@ -1,26 +1,9 @@
-import { Range } from "@/types";
-import InputRange from "react-input-range";
+import InputRange, { InputRangeProps } from "react-input-range";
 import "react-input-range/lib/css/index.css";
 import "./stlye.module.css";
-type Props = {
-  value: number | Range;
-  onChange: (value: number | Range) => void;
-  params: any;
-  max: number;
-  min: number;
-};
 
-const RangeInput = (props: Props) => {
-  const { value, onChange, max, min, ...params } = props;
-  return (
-    <InputRange
-      maxValue={max}
-      minValue={min}
-      value={value}
-      onChange={onChange}
-      {...params}
-    />
-  );
+const RangeInput = (props: InputRangeProps) => {
+  return <InputRange {...props} />;
 };
 
 export default RangeInput;
