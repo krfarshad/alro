8803f08b6f445f265334b2ac9d262e075a5a562e
krfarshad <krfarshad@gmail.com>
2023-12-29 16:40:52 +0330

    refactor: edit project functions

--- src/features/projects/api/projectList.ts
+++ src/features/projects/api/projectList.ts
@@ -1,13 +1,15 @@
+import { delay } from "@/utils/delay";
 import { ProjectFilter, ProjectItem } from "..";
 import { projects as allProjects } from "../lib/data/projects";
 import { filterProject } from "../utils/filterProject";
 
 // just for simulation api calling
-export const projectList = (
+export const projectList = async (
   filters?: ProjectFilter | null
-): ProjectItem[] | null => {
+): Promise<ProjectItem[] | null> => {
   if (filters) {
-    const projects = filterProject(filters);
+    const projects = await filterProject(filters);
+    await delay(1000);
     return projects;
   }
   return allProjects;
