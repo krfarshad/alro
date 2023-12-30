0ef53e8c80d66c0594ac14de6ffafbfba3970c76
Farshad <krfarshad@gmail.com>
2023-12-30 15:31:47 +0330

    refactor: edit ui error component

--- src/features/errorBoundary/components/ErrorBoundary.tsx
+++ src/features/errorBoundary/components/ErrorBoundary.tsx
@@ -1,5 +1,17 @@
+import { Row, Text } from "@/components";
+import { Danger } from "iconsax-react";
+
 const ErrorBoundary = () => {
-  return <div>Sth went wrong !!</div>;
+  return (
+    <Row className="p-4 text-center h-96 items-center justify-center">
+      <div>
+        <Danger size="60" className="!text-red-600 mx-auto" />
+        <Text className="text-center !text-red-600 !text-lg">
+          ooops? <br /> Something went wrong :(
+        </Text>
+      </div>
+    </Row>
+  );
 };
 
 export default ErrorBoundary;
