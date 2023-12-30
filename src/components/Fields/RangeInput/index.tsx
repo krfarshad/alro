f19d97e0d1f2fad7407a29798f2fd01a8c2f0be4
Farshad <krfarshad@gmail.com>
2023-12-30 11:34:43 +0330

    style: implement custom style on range input

--- src/components/Fields/RangeInput/index.tsx
+++ src/components/Fields/RangeInput/index.tsx
@@ -1,6 +1,5 @@
 import InputRange, { InputRangeProps } from "react-input-range";
 import "react-input-range/lib/css/index.css";
-import "./stlye.module.css";
 
 const RangeInput = (props: InputRangeProps) => {
   return <InputRange {...props} />;
