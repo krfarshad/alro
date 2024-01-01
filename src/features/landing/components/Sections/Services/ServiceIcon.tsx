85c14e69fb7afd75a62c546da4e6ec5abb7b52cd
Farshad <krfarshad@gmail.com>
2024-01-01 13:02:48 +0330

    refactor: replace with useDynamicImage

--- src/features/landing/components/Sections/Services/ServiceIcon.tsx
+++ src/features/landing/components/Sections/Services/ServiceIcon.tsx
@@ -1,35 +1,16 @@
-import { loadDynamicImage } from "@/utils/loadDynamicImage";
-import { useEffect, useState } from "react";
 import placeholder from "@/assets/images/placeholder.png";
+import { useDynamicImage } from "@/hooks/useDynamicImage";
 type Props = {
   icon: string;
 };
 const ServiceIcon = (props: Props) => {
   const { icon } = props;
-  const [src, setSrc] = useState<string>("");
-
-  useEffect(() => {
-    const loadImage = async () => {
-      try {
-        const res = await loadDynamicImage(
-          `/src/features/landing/assets/${icon}`
-        );
-        setSrc(res);
-      } catch (err) {
-        throw new Error("Error loading dynamic image:");
-      }
-    };
-
-    if (!src) {
-      loadImage();
-    }
-  }, [src, icon]);
-
+  const [imageSrc] = useDynamicImage(icon);
   return (
     <div className="text-center">
       <img
         className="mx-auto inline-block mt-4 w-auto h-12"
-        src={src ? src : placeholder}
+        src={imageSrc ? imageSrc : placeholder}
         alt="icon"
       />
     </div>
