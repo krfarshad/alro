ce43d6c2791edbd69d5736f07fda8b16a3fbed0e
Farshad Karimi <krfarshad@gmail.com>
2024-10-25 11:58:03 +0330

    style: edit newsletter form style

--- src/components/Layout/Footer/Newsletter/NewsletterForm.tsx
+++ src/components/Layout/Footer/Newsletter/NewsletterForm.tsx
@@ -2,15 +2,15 @@ import { Text } from "@/components";
 import { useState } from "react";
 
 const NewsletterForm = () => {
-  const [submited, setSubmited] = useState<boolean>(false);
+  const [submit, setSubmit] = useState<boolean>(false);
   const handleSubmit = () => {
-    setSubmited(true);
+    setSubmit(true);
   };
 
   return (
     <>
-      {submited ? (
-        <Text className="!text-white bg-green-700 rounded-sm text-center">
+      {submit ? (
+        <Text className="!text-white bg-green-800 text-center p-4 rounded-md">
           Thank you for subscribing to our newsletter! You will receive a
           confirmation email shortly. Please click the confirmation link to
           complete the subscription process.
@@ -23,13 +23,13 @@ const NewsletterForm = () => {
           <input
             type="email"
             placeholder="Enter your email address..."
-            className="shadow-sm text-sm w-[70%] outline-none shadow-slate-300 placeholder:text-gray-400 font-light p-2 rounded-l-sm"
+            className="shadow-sm text-sm w-[70%] outline-none shadow-slate-300 border border-gray-200 text-dark placeholder:text-gray-500 font-light p-2 rounded-l-sm"
             required
           />
           <input
             type="submit"
             value="Submit"
-            className="w-20 text-white text-sm border border-primary bg-primary  shadow-slate-300  rounded-r-sm p-2 font-light"
+            className="w-20 text-white text-sm border border-primary bg-primary  shadow-slate-300 hover:bg-amber-700 rounded-r-sm p-2 font-light cursor-pointer transition-all hover:transition-all"
           />
         </form>
       )}
